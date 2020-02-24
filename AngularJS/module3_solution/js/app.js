75086d635a50b737790589168461139714aa7fd2
kpeitsch <61016563+kpeitsch@users.noreply.github.com>
2020-02-24 10:02:38 +0100

    Add files via upload

--- AngularJS/module3_solution/js/app.js
+++ AngularJS/module3_solution/js/app.js
@@ -10,8 +10,8 @@ function FoundItemsDirective() {
   var ddo = {
     templateUrl: 'foundItems.html',
     scope: {
-      foundItems: '<',
-      onRemove: '&'
+      foundItems: '<' //,
+      // onRemove: '&'
     },
   };
 
